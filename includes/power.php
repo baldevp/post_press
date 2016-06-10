e803939c3314d1364dc752f03a2b985011426ac1
Baldev Patwari <baldevp@gmail.com>
2016-06-10 14:42:05 +0530

    Completed removeuser module

--- includes/power.php
+++ includes/power.php
@@ -1,14 +1,5 @@
 <?php
-//User's power in json
-$query="select function_name,functions.function_id,function_link,fc_name from user_power inner join functions on user_power.function_id=functions.function_id inner join func_category on  functions.func_category=func_category.fc_id  where user_id=".$_COOKIE['user_id'];
-		$result1=mysqli_query($db,$query);
-		$json='[';
-		while($r=mysqli_fetch_array($result1)){
-			$json=$json.'{"fn":"'.$r['function_name'].'","fi":"'.$r['function_id'].'","fl":"'.$r['function_link'].'","fc":"'.$r['fc_name'].'"},';
-		}
-		$json=substr($json, 0, -1);
-		$json=$json.']'; 
-
+//Check if user can really access that page
 function power($db){
 	$link=$_SERVER['REQUEST_URI'];
 	$link=basename($link).PHP_EOL;
@@ -21,6 +12,7 @@ function power($db){
 		header('Location: .');
 }
 
+//Gets the list of all the functions possible 
 function functions($db){
 	$query="select * from func_category inner join functions on func_category.fc_id=functions.func_category order by func_category";
 	$result=mysqli_query($db,$query);
