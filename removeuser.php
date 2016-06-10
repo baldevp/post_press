72abdc9f2d2e7bf8928504373f2b638fe3e83021
Baldev Patwari <baldevp@gmail.com>
2016-06-10 13:47:30 +0530

    Added nav and inproved UI

---
+++ removeuser.php
@@ -0,0 +1,12 @@
+<?php
+$title="Remove Users";
+include_once('includes/head.php');
+include_once('includes/power.php');
+include_once('includes/nav.php');
+power($db);
+?>
+
+<?php
+include_once('includes/script.php');
+include_once('includes/foot.php'); 
+?>
--- verifyusers.php
+++ verifyusers.php
@@ -2,12 +2,15 @@
 $title="Verify Users";
 include_once('includes/head.php');
 include_once('includes/power.php');
+include_once('includes/nav.php');
 power($db);
 ?>
-<div id="login_outer">
-<div class="container">
 <?php
-$query="select * from user where verified=0";
+if(isset($_POST['submit1'])){
+	$id=$_POST['user_id'];
+	$query="delete from user where user_id=$id";
+	mysqli_query($db,$query);
+}
 if(isset($_POST['submit'])){
 	$id=$_POST['user_id'];
 	$query2="update user set verified=1 where user_id=$id";
@@ -23,6 +26,7 @@ if(isset($_POST['submit'])){
 	<div class="col-md-6">
 		<?php echo $result['email'] ?>
 	</div>
+	<hr>
 	</div>
 	<form method="POST">
 
@@ -36,7 +40,7 @@ if(isset($_POST['submit'])){
 	?>
 	<div class="col-md-4">
 		<label class="checkbox-inline">
-			<input type="checkbox" value="<?php echo $x['function_id'] ?>" name="func[]" >
+			<input type="checkbox" value="<?php echo $x['function_id'] ?>" name="func[]" ><?php echo $x['function_name'] ?>
 		</label>
 	</div>
 	<?php
@@ -47,16 +51,44 @@ if(isset($_POST['submit'])){
     if($num%3!=0)
     	echo '</div>';
      ?>
+     <hr>
+     <div class="row">
+     <div class="col-md-4 col-md-offset-4">
+    <input type="hidden" name="user_id" value="<?php echo $id ?>" >
     <button type="submit" class="btn btn-success" name="submit2">Update</button>
+    </div>
+    <div class="col-md-4">
     <button type="submit" class="btn btn-primary" name="submit3">Cancel</button>
+    </div>
+    </div>
+    <hr>
     </form>
     <?php
 }
+if(isset($_POST['submit2'])){
+	$id=$_POST['user_id'];
+	foreach($_POST['func'] as $func){
+		$query="insert into user_power(user_id,function_id) values($id,$func)";
+		mysqli_query($db,$query) or die("error querying db");
+	}
+}
+$query="select * from user where verified=0";
 $result=mysqli_query($db,$query);
-
+while($row=mysqli_fetch_array($result)){
 ?>
+<form method="post" class="form1">
+<input type="hidden" name="user_id" value="<?php echo $row['user_id']; ?>">
+<div class="row">
+<div class="col-md-4"><?php echo $row['fname'].' '.$row['lname']; ?></div>
+<div class="col-md-4"><?php echo $row['email']; ?></div>
+<div class="col-md-2"><button type="submit" name="submit" class="btn btn-success">Verify</button></div>
+<div class="col-md-2"><button type="submit" name="submit1" class="btn btn-danger">Reject</button></div>
 </div>
-</div>
+<hr>
+</form>
+<?php
+} 
+?>
 <?php
 include_once('includes/script.php');
 include_once('includes/foot.php'); 
